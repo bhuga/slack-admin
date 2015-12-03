0101bc046276fc111e0ee7b351ff3766deffa6d3
Ben Lavender <bhuga@github.com>
2015-12-03 00:55:30 -0600

    more scaffolding

--- get-retention.sh
+++ get-retention.sh
@@ -2,5 +2,7 @@
 
 export PATH=./slimerjs:$PATH
 export PATH=./node_modules/.bin:$PATH
+export PATH=./bin:$PATH
+export SLIMERJS_EXECUTABLE=./bin/slimerjs-wrapper
 
 casperjs --engine=slimerjs get-retention.js $@ | grep -v "Vector smash protection is enabled."
